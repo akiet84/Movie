b031ac2f093c0ad4c38b6886a330b133cafd0688
akiet84 <kietanhvunguyen@gmail.com>
2025-08-12 17:13:25 +0700

    API

--- src/pages/movie-detail/index.jsx
+++ src/pages/movie-detail/index.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function MovieDeatail() {
   //xác định id là gì
   const { movieId } = useParams();
-
+  const [movieDetail, setMovieDetail] = useState();
   console.log(movieId);
 
   //https://api.themoviedb.org/3/movie/1087192?api_key=a10ee5569194b352bcca20840b7f8a32
@@ -19,9 +19,15 @@ function MovieDeatail() {
       `https://api.themoviedb.org/3/movie/${movieId}?api_key=a10ee5569194b352bcca20840b7f8a32`
     );
     console.log(response.data);
+    setMovieDetail(response.data);
   };
 
-  return <div>hihi</div>;
+  return <div style={{
+    color: "white",
+  }}>
+    {movieDetail?.original_title}
+    {movieDetail?.overview}
+  </div>;
 }
 
 export default MovieDeatail;
